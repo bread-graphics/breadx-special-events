e23824c2a3574cb5dfd0f26fb3fd250572a25b1d
jtnunley <jtnunley01@gmail.com>
2022-05-24 11:36:26 -0700

    rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,18 @@
 // MIT/Apache2 License
 
 //! Implements the "special events" pattern for [`breadx`].
-//! 
+//!
 //! Some extensions to X11 mandate a pattern known as "special events", where
 //! certain events are sorted into queues for libraries to process. It's only
 //! ever used in a couple of extensions, and checking for them is not idiomatic
 //! to do on the hot path for event processing. Therefore, [`breadx`] does not
 //! do it by default.
-//! 
+//!
 //! This module provides a way to process special events. The [`SpecialEventDisplay`]
 //! type is a wrapper around a `Display` that provides queues for special events.
 //! Queues can be registered or deregistered, and then polled or waited on, similar
 //! to regular events.
-//! 
+//!
 //! [`SpecialEventDisplay`] can be created through its universal `From` implementation.
 
 #![no_std]
@@ -20,18 +20,22 @@
 extern crate alloc;
 
 use alloc::{boxed::Box, collections::VecDeque, vec::Vec};
-use breadx::{Result, protocol::Event, display::{DisplayBase, Display, RawReply, RawRequest}};
+use breadx::{
+    display::{Display, DisplayBase, RawReply, RawRequest},
+    protocol::Event,
+    Result,
+};
 use slab::Slab;
 
 #[cfg(feature = "async")]
-use breadx::display::{AsyncStatus, CanBeAsyncDisplay, AsyncDisplay, AsyncDisplayExt};
+use breadx::display::{AsyncDisplay, AsyncDisplayExt, AsyncStatus, CanBeAsyncDisplay};
 #[cfg(feature = "async")]
 use core::task::Context;
 
 /// A special type of [`Display`] that provides queues for special events.
-/// 
+///
 /// See the [module level documentation](index.html) for more information.
-/// 
+///
 /// [`Display`]: breadx::display::Display
 pub struct SpecialEventDisplay<Dpy: ?Sized> {
     /// Queue for normal events.
@@ -74,7 +78,7 @@ impl<Dpy: ?Sized> AsMut<Dpy> for SpecialEventDisplay<Dpy> {
     }
 }
 
-impl<Dpy> SpecialEventDisplay<Dpy>  {
+impl<Dpy> SpecialEventDisplay<Dpy> {
     /// Get the inner display type.
     pub fn into_inner(self) -> Dpy {
         self.display
@@ -83,11 +87,11 @@ impl<Dpy> SpecialEventDisplay<Dpy>  {
 
 impl<Dpy: ?Sized> SpecialEventDisplay<Dpy> {
     /// Create a new special event queue with the given classifier.
-    /// 
+    ///
     /// Returns the key to be used to poll the event queue.
     pub fn create_special_event_queue(
         &mut self,
-        classifier: impl FnMut(&Event) -> bool + Send + Sync + 'static
+        classifier: impl FnMut(&Event) -> bool + Send + Sync + 'static,
     ) -> usize {
         // create a new queue in the slab
         let key = self.special_event_queues.insert(VecDeque::new());
@@ -128,7 +132,10 @@ impl<Dpy: ?Sized> SpecialEventDisplay<Dpy> {
 
 impl<Dpy: DisplayBase + ?Sized> SpecialEventDisplay<Dpy> {
     /// Poll for an event from the inner display until we find one that we want.
-    fn poll_for_some_event(&mut self, mut until: impl FnMut(&mut Self) -> Option<Event>) -> Result<Option<Event>> {
+    fn poll_for_some_event(
+        &mut self,
+        mut until: impl FnMut(&mut Self) -> Option<Event>,
+    ) -> Result<Option<Event>> {
         loop {
             // poll for event from the inner display
             let event = match self.display.poll_for_event()? {
@@ -152,7 +159,10 @@ impl<Dpy: DisplayBase + ?Sized> SpecialEventDisplay<Dpy> {
 
 impl<Dpy: Display + ?Sized> SpecialEventDisplay<Dpy> {
     /// Wait for an event from the inner display.
-    fn wait_for_some_event(&mut self, mut until: impl FnMut(&mut Self) -> Option<Event>) -> Result<Event> {
+    fn wait_for_some_event(
+        &mut self,
+        mut until: impl FnMut(&mut Self) -> Option<Event>,
+    ) -> Result<Event> {
         loop {
             // poll for event from the inner display
             let event = self.display.wait_for_event()?;
@@ -204,7 +214,7 @@ impl<D: DisplayBase + ?Sized> DisplayBase for SpecialEventDisplay<D> {
         self.display.poll_for_reply_raw(seq)
     }
 }
- 
+
 impl<D: Display + ?Sized> Display for SpecialEventDisplay<D> {
     fn send_request_raw(&mut self, req: RawRequest) -> Result<u64> {
         self.display.send_request_raw(req)
@@ -237,27 +247,35 @@ impl<D: Display + ?Sized> Display for SpecialEventDisplay<D> {
 
 #[cfg(feature = "async")]
 impl<D: CanBeAsyncDisplay + ?Sized> CanBeAsyncDisplay for SpecialEventDisplay<D> {
-    fn format_request(&mut self, req: &mut RawRequest, ctx: &mut Context< '_>) ->Result<AsyncStatus<u64>> {
+    fn format_request(
+        &mut self,
+        req: &mut RawRequest,
+        ctx: &mut Context<'_>,
+    ) -> Result<AsyncStatus<u64>> {
         self.display.format_request(req, ctx)
     }
 
-    fn try_flush(&mut self, ctx: &mut Context< '_>) ->Result<AsyncStatus<()>> {
+    fn try_flush(&mut self, ctx: &mut Context<'_>) -> Result<AsyncStatus<()>> {
         self.display.try_flush(ctx)
     }
 
-    fn try_generate_xid(&mut self, ctx: &mut Context< '_>) ->Result<AsyncStatus<u32>> {
+    fn try_generate_xid(&mut self, ctx: &mut Context<'_>) -> Result<AsyncStatus<u32>> {
         self.display.try_generate_xid(ctx)
     }
 
-    fn try_maximum_request_length(&mut self, ctx: &mut Context< '_>) ->Result<AsyncStatus<usize>> {
+    fn try_maximum_request_length(&mut self, ctx: &mut Context<'_>) -> Result<AsyncStatus<usize>> {
         self.display.try_maximum_request_length(ctx)
     }
-    
-    fn try_send_request_raw(&mut self, req: &mut RawRequest, ctx: &mut Context< '_>) ->Result<AsyncStatus<()>> {
+
+    fn try_send_request_raw(
+        &mut self,
+        req: &mut RawRequest,
+        ctx: &mut Context<'_>,
+    ) -> Result<AsyncStatus<()>> {
         self.display.try_send_request_raw(req, ctx)
     }
 
-    fn try_wait_for_event(&mut self, ctx: &mut Context< '_>) ->Result<AsyncStatus<Event>> {
+    fn try_wait_for_event(&mut self, ctx: &mut Context<'_>) -> Result<AsyncStatus<Event>> {
         loop {
             let event = match self.display.try_wait_for_event(ctx)? {
                 AsyncStatus::Ready(event) => event,
@@ -272,7 +290,11 @@ impl<D: CanBeAsyncDisplay + ?Sized> CanBeAsyncDisplay for SpecialEventDisplay<D>
         }
     }
 
-    fn try_wait_for_reply_raw(&mut self, seq:u64, ctx: &mut Context< '_>) ->Result<AsyncStatus<RawReply>> {
-        self.display.try_wait_for_reply_raw(seq, ctx) 
+    fn try_wait_for_reply_raw(
+        &mut self,
+        seq: u64,
+        ctx: &mut Context<'_>,
+    ) -> Result<AsyncStatus<RawReply>> {
+        self.display.try_wait_for_reply_raw(seq, ctx)
     }
 }
